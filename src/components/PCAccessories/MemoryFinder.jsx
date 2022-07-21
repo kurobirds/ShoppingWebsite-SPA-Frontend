e7888bb1334d6baf68615d9914953bcf5f4979fb
Sang <sang024854@gmail.com>
2022-07-22 00:19:59 +0700

    update to localhost new component

--- src/components/PCAccessories/MemoryFinder.jsx
+++ src/components/PCAccessories/MemoryFinder.jsx
@@ -1,27 +1,17 @@
 import React, { Component } from "react";
-import Iframe from "react-iframe";
 
 export default class MemoryFinder extends Component {
 	render() {
 		return (
 			<div style={{
-				height: "70vh",
+				height: "76vh",
 			}}>
-				<Iframe
-					url="https://www.corsair.com/us/en/memoryfinderForMindFactory/systems&amp;motherboard"
+				<iframe src="https://local.corsair.com:9002/de/de/memoryfinderForMindFactory/systems&motherboard?type=motherboard" frameborder="0"
 					width="100%"
 					height="100%"
-					id="iFrameResizer0"
-					scrolling="no"
 					className="MemoryFinder"
-					display="initial"
-					position="relative"
-					styles={{
-						border: "1px solid rgb(206, 206, 206)",
-						overflow: "hidden",
-						height: "411.375px",
-					}}
-				/>
+					id="iFrameResizer0"
+					title="MemoryFinder"></iframe>
 			</div>
 		);
 	}
